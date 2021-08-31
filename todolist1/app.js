f2c73dd732c7866d29aef10f28417418cb93a627
choi sunmin <1csm9898@adup.kr>
2021-08-31 18:27:08 +0900

    Add : 8/31 ver

--- todolist1/app.js
+++ todolist1/app.js
@@ -1,29 +1,36 @@
+"use strict";
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 var toDos = [];
+let workItems = [];
 //scope
 var toDo = "";
 //항상 view engine은 앱 상수 선언직후 작성=>앱이 사용되었다는 오류 안생김
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  var today = new Date();
-  var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-
-  var day = today.toLocaleString("kr", options);
-  res.render("list", { kindOfDay: day, toDos: toDos });
+  let day = date.getDay();
+  res.render("list", { listTitle: day, lists: toDos });
 });
 
 app.post("/", function (req, res) {
-  toDos.push(req.body.toDo);
-  res.redirect("/");
+  if (req.body.button === "work List") {
+    workItems.push(req.body.toDo);
+    res.redirect("/work");
+  } else {
+    toDos.push(req.body.toDo);
+    res.redirect("/");
+  }
 });
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "work List", lists: workItems });
+});
+
 app.listen(3000, function () {
   console.log("port 3000 is running");
 });
--- todolist2/index.js
+++ todolist2/index.js
@@ -1,19 +1,31 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-var toDos = [];
-var toto = "";
+const date = require(__dirname + "/date.js");
+const toDos = [];
+let toDo = "";
+const works = [];
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.get("/", function (res, req) {
-  req.render("todo", { toDos: toDos });
+app.get("/", function (req, res) {
+  let day = date.getDate();
+  res.render("todo", { listTitle: day, lists: toDos });
 });
 
-app.post("/", function (res, req) {
-  const toDo = res.body.toDo;
-  toDos.push(toDo);
-  req.redirect("/");
+app.post("/", function (req, res) {
+  item = req.body.toDo;
+  if (req.body.button === "work List") {
+    works.push(item);
+    res.redirect("/work");
+  } else {
+    toDos.push(item);
+    res.redirect("/");
+  }
+});
+app.get("/work", function (req, res) {
+  res.render("todo", { listTitle: "work List", lists: works });
 });
 
 app.listen(3000, function () {
