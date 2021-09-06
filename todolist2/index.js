f4ba20d00d9d145f5499c0f3fb0c0cca268c8a69
choi sunmin <1csm9898@adup.kr>
2021-09-06 17:03:55 +0900

    Add : 9/6

--- todolist2/index.js
+++ todolist2/index.js
@@ -1,31 +1,103 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
-const toDos = [];
-let toDo = "";
+const mongoose = require("mongoose");
+
 const works = [];
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+const itemSchema = new mongoose.Schema({
+  name: String,
+});
+const Item = mongoose.model("Item", itemSchema);
+const item1 = new Item({
+  name: "할일 1",
+});
+const item2 = new Item({
+  name: "할일 2",
+});
+const item3 = new Item({
+  name: "할일 3",
+});
+const defaultItems = [item1, item2, item3];
+
+const listSchema = new mongoose.Schema({
+  name: {
+    required: true,
+    type: String,
+  },
+  items: [itemSchema],
+});
+const List = mongoose.model("List", listSchema);
+
+// Item.insertMany(defaultItems, function (err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("정상적 추가");
+//   }
+// });
+
 app.get("/", function (req, res) {
-  let day = date.getDate();
-  res.render("todo", { listTitle: day, lists: toDos });
+  Item.find({}, function (err, items) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("todo", { listTitle: "Today", lists: items });
+    }
+  });
+});
+
+app.get("/:param", function (req, res) {
+  const customName = req.params.param;
+  List.findOneAndDelete({ name: customName }, function (err, list) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (!list) {
+        List.find({ name: customName }, function (err, list) {
+          if (!err) {
+            console.log(list);
+          }
+        });
+      } else {
+        const list = new List({
+          name: customName,
+          items: defaultItems,
+        });
+        list.save();
+      }
+    }
+  });
 });
 
 app.post("/", function (req, res) {
-  item = req.body.toDo;
+  const item = req.body.toDo;
   if (req.body.button === "work List") {
     works.push(item);
     res.redirect("/work");
   } else {
-    toDos.push(item);
+    const newItem = new Item({
+      name: item,
+    });
+    newItem.save();
     res.redirect("/");
   }
 });
 app.get("/work", function (req, res) {
   res.render("todo", { listTitle: "work List", lists: works });
+});
+app.post("/delete", function (req, res) {
+  const deleteItem = req.body.checkbox;
+  Item.findByIdAndRemove(deleteItem, function (err) {
+    if (!err) {
+      console.log("정상적 삭제");
+    }
+  });
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
